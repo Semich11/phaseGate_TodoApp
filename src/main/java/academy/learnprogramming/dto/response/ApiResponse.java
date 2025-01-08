a4bec63259462b62602e690ba46f4b17056a6bec
semich11 <chima11christopher@gmail.com>
2025-01-08 10:24:48 +0100

    First push.

---
+++ src/main/java/academy/learnprogramming/dto/response/ApiResponse.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse {
+
+    private boolean isSuccessful;
+    private Object data;
+}
